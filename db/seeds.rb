21b553103ecc39a198a5c321c55e507e084e149c
Louis Guyot <louis.guyot.pro@gmail.com>
2022-11-15 17:01:36 +0100

    done but ugly

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,17 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+puts 'Creating 10 fake articles...'
+10.times do
+  article = Article.new(
+    title: "#{Faker::Emotion.adjective} #{Faker::Science.science(:natural, :applied)}",
+    content: Faker::Lorem.sentence(word_count: 30, supplemental: true, random_words_to_add: 8)
+  )
+  article.save
+end
+puts 'DONE!'
